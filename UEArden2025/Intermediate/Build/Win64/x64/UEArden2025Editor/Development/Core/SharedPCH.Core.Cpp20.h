23dee103d83fb2127bbfe5285b2005b3d9bdcc93
Aseuno <aseunopro11@gmail.com>
2025-11-13 00:58:01 -0600

    Inicio de Nuevo
    aaaaaaaaaaaaa

--- UEArden2025/Intermediate/Build/Win64/x64/UEArden2025Editor/Development/Core/SharedPCH.Core.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "C:/Users/aseun/OneDrive/Documentos/Cursos C++/UEArden2025/UEArden2025/Intermediate/Build/Win64/x64/UEArden2025Editor/Development/Core/SharedDefinitions.Core.Cpp20.h"
-#include "Runtime/Core/Public/CoreSharedPCH.h"
--- UEArden2025/Intermediate/Build/Win64/x64/UEArden2025Editor/Development/UnrealEd/SharedPCH.UnrealEd.RTTI.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "C:/Users/aseun/OneDrive/Documentos/Cursos C++/UEArden2025/UEArden2025/Intermediate/Build/Win64/x64/UEArden2025Editor/Development/UnrealEd/SharedDefinitions.UnrealEd.RTTI.Cpp20.h"
-#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
